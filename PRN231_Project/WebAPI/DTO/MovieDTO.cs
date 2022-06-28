fee4b98312294673a3f3412104f9593246edcd03
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-29 04:29:23 +0700

    create Movie DTO

---
+++ PRN231_Project/WebAPI/DTO/MovieDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.DTO
+{
+    public class MovieDTO
+    {
+       
+        public int MovieId { get; set; }
+        
+        public string MovieName { get; set; }
+
+        public string Path { get; set; }
+
+        public string Description { get; set; }
+
+        public string Duration { get; set; }
+
+        public int Rated { get; set; }
+
+        public string PublishedYear { get; set; }
+
+        public string Country { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
