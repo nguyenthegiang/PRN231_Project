b2eeb086b5eafd1d4f4c2459adf461a0251caa6a
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-29 04:28:40 +0700

    add class Movie

---
+++ PRN231_Project/WebAPI/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Movie
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MovieId { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string MovieName { get; set; }
+        
+        public string Path { get; set; }
+        
+        public string Description { get; set; }
+       
+        public string Duration { get; set; }
+        
+        public int Rated { get; set; }
+        
+        public string PublishedYear { get; set; }
+        
+        public string Country { get; set; }
+        
+        public string Image { get; set; }
+
+
+    }
+}
