e719b11fdf357cc57c537f3150abf688fbb8550a
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-29 04:28:55 +0700

    setup starup

--- PRN231_Project/WebAPI/Startup.cs
+++ PRN231_Project/WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Models;
 
 namespace WebAPI
 {
@@ -28,6 +30,8 @@ namespace WebAPI
         {
 
             services.AddControllers();
+            services.AddCors();
+            services.AddDbContext<MyDbContext>(otp => otp.UseSqlServer(Configuration.GetConnectionString("DBContext")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
@@ -43,6 +47,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder => builder.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
