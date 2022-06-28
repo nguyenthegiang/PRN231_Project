2956e2a852305b5fe8b1c064fce23cebb8eef7d1
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-29 04:29:38 +0700

    MapperProfile

---
+++ PRN231_Project/WebAPI/DTO/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DTO;
+using WebAPI.Models;
+
+namespace BT2TrenLop.DTO
+{
+    public class MapperProfile :Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Movie, MovieDTO>();
+            
+        }
+    }
+}
