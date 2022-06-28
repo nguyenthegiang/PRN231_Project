987d7a830bf91efe0a05bcbd965153cd29cee4ea
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-06-28 22:01:23 +0700

    [Watch Video]: API Controller

---
+++ PRN231_Project/WebAPI/Controllers/VideoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public VideoController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("GetVideoContent")]
+        public async Task<IActionResult> GetVideoContent(string fileName)
+        {
+            string path = Path.Combine(_hostingEnvironment.WebRootPath, "Video/") + fileName;
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 65536, FileOptions.Asynchronous | FileOptions.SequentialScan))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/octet-stream", Path.GetFileName(path), true); //enableRangeProcessing = true
+        }
+    }
+}
