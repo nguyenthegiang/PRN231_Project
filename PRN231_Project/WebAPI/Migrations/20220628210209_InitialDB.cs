2ebc65b2463c108c0b787e3f655743ab76af5308
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-29 04:30:18 +0700

    Db code first

---
+++ PRN231_Project/WebAPI/Migrations/20220628210209_InitialDB.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Duration = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rated = table.Column<int>(type: "int", nullable: false),
+                    PublishedYear = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Country", "Description", "Duration", "Image", "MovieName", "Path", "PublishedYear", "Rated" },
+                values: new object[] { 1, null, null, null, null, "Iron Man", null, null, 0 });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Country", "Description", "Duration", "Image", "MovieName", "Path", "PublishedYear", "Rated" },
+                values: new object[] { 2, null, null, null, null, "Spider Man", null, null, 0 });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Country", "Description", "Duration", "Image", "MovieName", "Path", "PublishedYear", "Rated" },
+                values: new object[] { 3, null, null, null, null, "Ant Man", null, null, 0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ PRN231_Project/WebAPI/Models/MyDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace WebAPI.Models
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext()
+        {
+        }
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DBContext"));
+        }
+        public virtual DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder optionsBuilder)
+        {
+            optionsBuilder.Entity<Movie>().HasData(
+                new Movie { MovieId = 1, MovieName = "Iron Man" },
+                new Movie { MovieId = 2, MovieName = "Spider Man" },
+                new Movie { MovieId = 3, MovieName = "Ant Man" }
+            );
+        }
+    }
+}
+
