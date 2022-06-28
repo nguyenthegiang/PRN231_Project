5f5ebe897be9646afd47716d24c2abcf5e11df1a
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-29 04:30:38 +0700

    Create API Controller

---
+++ PRN231_Project/WebAPI/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BT2TrenLop.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DTO;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MyDbContext context;
+        private MapperConfiguration config;
+        private IMapper mapper;
+
+        public MovieController(MyDbContext _context)
+        {
+            context = _context;
+            config = new MapperConfiguration(cfg => cfg.AddProfile(new MapperProfile()));
+            mapper = config.CreateMapper();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMovie()
+        {
+            List<MovieDTO> movieDTOs;
+            movieDTOs = context.Movies.ProjectTo<MovieDTO>(config).ToList();
+            if (movieDTOs == null)
+            {
+                return NotFound(); //Response with status code: 404
+            }
+            return Ok(movieDTOs);
+        }
+    }
+}
