ac12dcf1278e0b3341799c28f5249f280d8c6537
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-06-29 20:27:51 +0700

    [Database Design] Add: Actor

---
+++ PRN231_Project/WebAPI/Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Actor
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ActorId { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string ActorName { get; set; }
+    }
+}
