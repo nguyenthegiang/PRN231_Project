af3f92c967b3bfa77ac22a3cabdb48326b8632c0
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-30 13:05:46 +0700

    Add ICategoryRepository

---
+++ PRN231_Project/WebAPI/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DTO;
+
+namespace WebAPI.IRepository
+{
+    public interface ICategoryRepository
+    {
+        List<CategoryDTO> GetListCategories();
+    }
+}
