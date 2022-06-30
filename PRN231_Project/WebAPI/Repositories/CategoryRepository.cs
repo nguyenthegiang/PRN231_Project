462b06eafa325e90416733b7078e9117f25957a4
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-06-30 13:08:04 +0700

    create CategoryRepository

---
+++ PRN231_Project/WebAPI/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DataAccess;
+using WebAPI.DTO;
+using WebAPI.IRepository;
+
+namespace WebAPI.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public List<CategoryDTO> GetListCategories() => CategoryDAO.GetCategories();
+    }
+}
