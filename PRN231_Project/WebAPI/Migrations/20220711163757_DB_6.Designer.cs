f71f423840eb7ce2c6a33728625bce28a169d202
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-07-11 23:38:49 +0700

    [Database Seeding]: Actor

--- PRN231_Project/WebAPI/Migrations/20220711163757_DB_6.Designer.cs
+++ PRN231_Project/WebAPI/Migrations/20220711163757_DB_6.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPI.Models;
 
 namespace WebAPI.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220711163757_DB_6")]
+    partial class DB_6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PRN231_Project/WebAPI/Migrations/20220711163757_DB_6.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class DB_6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Actor",
+                columns: new[] { "ActorId", "ActorName" },
+                values: new object[,]
+                {
+                    { 7, "John Favreau" },
+                    { 8, "Stan Lee" },
+                    { 9, "Gwyneth Paltrow" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { 6, "Family" },
+                    { 7, "Insect" },
+                    { 8, "Blood" },
+                    { 9, "Robot" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Actor",
+                keyColumn: "ActorId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Actor",
+                keyColumn: "ActorId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Actor",
+                keyColumn: "ActorId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 9);
+        }
+    }
+}
