61403bfb929c18801cd84dd083f9d567a61a6a31
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-07-11 23:59:04 +0700

    [Database Seeding]: Movie

---
+++ PRN231_Project/WebAPI/Migrations/20220711165815_DB_7.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class DB_7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby. The character made his first appearance in Tales of Suspense #39 (cover dated March 1963), and received his own title in Iron Man #1 (May 1968). Also in 1963, the character founded the Avengers alongside Thor, Ant-Man, Wasp and the Hulk.", 126, 2008, 10 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2,
+                columns: new[] { "Description", "Duration", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "Spider-Man: No Home is a 2021 American superhero film based on the Marvel Comics character Peter Parker, co-produced by Columbia Pictures and Marvel Studios and distributed by Sony Pictures Releasing", 148, "Spider Man: No way home", 2021, 13 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "Ant-Man is a 2015 American superhero film based on the Marvel Comics characters of the same name: Scott Lang and Hank Pym. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, it is the 12th installment in the Marvel Cinematic Universe film series.", 118, 2015, 9 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 4,
+                columns: new[] { "Description", "Duration", "PublishedYear" },
+                values: new object[] { "Black Panther: Black Panther is an American film based on the Marvel Comics superhero character of the same name, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures.", 135, 2018 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 5,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "Black Widow is an American superhero film based on the Marvel Comics character of the same name, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures. Black Widow is the 24th film in the Marvel Cinematic Universe.", 133, 2021, 15 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 6,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "Captain America: The First Avenger is a 2011 American superhero film based on the Marvel Comics character Captain America. Produced by Marvel Studios and released by Paramount Pictures, it is the fifth film in the Marvel Cinematic Universe.", 124, 2011, 14 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 7,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "Captain Marvel is a 2019 American superhero film based on the Marvel Comics character Carol Danvers. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, the work is the 21st film in the Marvel Cinematic Universe.", 124, 2019, 14 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 8,
+                columns: new[] { "Description", "Duration", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "The Ultimate Wizard in the Chaos Multiverse is an American superhero film due out in 2022, based on the Marvel Comics character Doctor Strange.", 126, "Doctor Strange in the Multiverse of Madness", 2022, 16 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 9,
+                columns: new[] { "Description", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "Hulk (also known as The Hulk) is a 2003 American superhero film based on the Marvel Comics character of the same name, directed by Ang Lee and written by James Schamus, Michael France, and John Turman from a story by Schamus. It stars Eric Bana as Bruce Banner/Hulk, alongside Jennifer Connelly, Sam Elliott, Josh Lucas, and Nick Nolte. The film explores Bruce Banner's origins. After a lab accident involving gamma radiation, he transforms into a giant, green-skinned creature known as the Hulk whenever stressed or emotionally provoked. The United States military pursues him, and he clashes with his biological father, who has dark plans for his son.", "The Hulk", 2003, 15 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 10,
+                columns: new[] { "Description", "Duration", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "WandaVision is an American television miniseries written by Jac Schaeffer for the Disney+ streaming service, based on the Marvel Comics characters Wanda Maximoff / Scarlet Witch and Vision.", 129, "WandaVision", 2021, 18 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "Very cool", 120, 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2,
+                columns: new[] { "Description", "Duration", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "Very good", 120, "Spider Man", 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "So cool", 120, 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 4,
+                columns: new[] { "Description", "Duration", "PublishedYear" },
+                values: new object[] { "So cool", 120, 2001 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 5,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "So cool", 120, 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 6,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "So cool", 120, 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 7,
+                columns: new[] { "Description", "Duration", "PublishedYear", "Rated" },
+                values: new object[] { "So cool", 120, 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 8,
+                columns: new[] { "Description", "Duration", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "So cool", 120, "Doctor Strange", 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 9,
+                columns: new[] { "Description", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "So cool", "Hulk", 2001, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 10,
+                columns: new[] { "Description", "Duration", "MovieName", "PublishedYear", "Rated" },
+                values: new object[] { "So cool", 120, "Scarlet Witch", 2001, 12 });
+        }
+    }
+}
--- PRN231_Project/WebAPI/Models/MyDbContext.cs
+++ PRN231_Project/WebAPI/Models/MyDbContext.cs
@@ -173,28 +173,29 @@ namespace WebAPI.Models
                 }
             );
 
+            //************Movie
             optionsBuilder.Entity<Movie>().HasData(
                 new Movie
                 {
                     MovieId = 1,
                     MovieName = "Iron Man",
                     VideoPath = "Video/IronMan.mp4",
-                    Description = "Very cool",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby. The character made his first appearance in Tales of Suspense #39 (cover dated March 1963), and received his own title in Iron Man #1 (May 1968). Also in 1963, the character founded the Avengers alongside Thor, Ant-Man, Wasp and the Hulk.",
+                    Duration = 126,
+                    Rated = 10,
+                    PublishedYear = 2008,
                     Country = "America",
                     ImagePath = "Image/IronMan.jpg"
                 },
                 new Movie
                 {
                     MovieId = 2,
-                    MovieName = "Spider Man",
+                    MovieName = "Spider Man: No way home",
                     VideoPath = "Video/SpiderMan.mp4",
-                    Description = "Very good",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "Spider-Man: No Home is a 2021 American superhero film based on the Marvel Comics character Peter Parker, co-produced by Columbia Pictures and Marvel Studios and distributed by Sony Pictures Releasing",
+                    Duration = 148,
+                    Rated = 13,
+                    PublishedYear = 2021,
                     Country = "America",
                     ImagePath = "Image/SpiderMan.jpg"
                 },
@@ -203,10 +204,10 @@ namespace WebAPI.Models
                     MovieId = 3,
                     MovieName = "Ant Man",
                     VideoPath = "Video/AntMan.mp4",
-                    Description = "So cool",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "Ant-Man is a 2015 American superhero film based on the Marvel Comics characters of the same name: Scott Lang and Hank Pym. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, it is the 12th installment in the Marvel Cinematic Universe film series.",
+                    Duration = 118,
+                    Rated = 9,
+                    PublishedYear = 2015,
                     Country = "America",
                     ImagePath = "Image/AntMan.jpg"
                 },
@@ -215,10 +216,10 @@ namespace WebAPI.Models
                     MovieId = 4,
                     MovieName = "Black Panther",
                     VideoPath = "Video/BlackPanther.mp4",
-                    Description = "So cool",
-                    Duration = 120,
+                    Description = "Black Panther: Black Panther is an American film based on the Marvel Comics superhero character of the same name, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures.",
+                    Duration = 135,
                     Rated = 12,
-                    PublishedYear = 2001,
+                    PublishedYear = 2018,
                     Country = "America",
                     ImagePath = "Image/BlackPanther.jpg"
                 },
@@ -227,10 +228,10 @@ namespace WebAPI.Models
                     MovieId = 5,
                     MovieName = "Black Widow",
                     VideoPath = "Video/BlackWidow.mp4",
-                    Description = "So cool",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "Black Widow is an American superhero film based on the Marvel Comics character of the same name, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures. Black Widow is the 24th film in the Marvel Cinematic Universe.",
+                    Duration = 133,
+                    Rated = 15,
+                    PublishedYear = 2021,
                     Country = "America",
                     ImagePath = "Image/BlackWidow.jpg"
                 },
@@ -239,10 +240,10 @@ namespace WebAPI.Models
                     MovieId = 6,
                     MovieName = "Captain America",
                     VideoPath = "Video/CaptainAmerica.mp4",
-                    Description = "So cool",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "Captain America: The First Avenger is a 2011 American superhero film based on the Marvel Comics character Captain America. Produced by Marvel Studios and released by Paramount Pictures, it is the fifth film in the Marvel Cinematic Universe.",
+                    Duration = 124,
+                    Rated = 14,
+                    PublishedYear = 2011,
                     Country = "America",
                     ImagePath = "Image/CaptainAmerica.jpg"
                 },
@@ -251,45 +252,45 @@ namespace WebAPI.Models
                     MovieId = 7,
                     MovieName = "Captain Marvel",
                     VideoPath = "Video/CaptainMarvel.mp4",
-                    Description = "So cool",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "Captain Marvel is a 2019 American superhero film based on the Marvel Comics character Carol Danvers. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, the work is the 21st film in the Marvel Cinematic Universe.",
+                    Duration = 124,
+                    Rated = 14,
+                    PublishedYear = 2019,
                     Country = "America",
                     ImagePath = "Image/CaptainMarvel.jpg"
                 },
                 new Movie
                 {
                     MovieId = 8,
-                    MovieName = "Doctor Strange",
+                    MovieName = "Doctor Strange in the Multiverse of Madness",
                     VideoPath = "Video/DoctorStrange.mp4",
-                    Description = "So cool",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "The Ultimate Wizard in the Chaos Multiverse is an American superhero film due out in 2022, based on the Marvel Comics character Doctor Strange.",
+                    Duration = 126,
+                    Rated = 16,
+                    PublishedYear = 2022,
                     Country = "America",
                     ImagePath = "Image/DoctorStrange.jpg"
                 },
                 new Movie
                 {
                     MovieId = 9,
-                    MovieName = "Hulk",
+                    MovieName = "The Hulk",
                     VideoPath = "Video/Hulk.mp4",
-                    Description = "So cool",
+                    Description = "Hulk (also known as The Hulk) is a 2003 American superhero film based on the Marvel Comics character of the same name, directed by Ang Lee and written by James Schamus, Michael France, and John Turman from a story by Schamus. It stars Eric Bana as Bruce Banner/Hulk, alongside Jennifer Connelly, Sam Elliott, Josh Lucas, and Nick Nolte. The film explores Bruce Banner's origins. After a lab accident involving gamma radiation, he transforms into a giant, green-skinned creature known as the Hulk whenever stressed or emotionally provoked. The United States military pursues him, and he clashes with his biological father, who has dark plans for his son.",
                     Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Rated = 15,
+                    PublishedYear = 2003,
                     Country = "America",
                     ImagePath = "Image/Hulk.jpg"
                 }, new Movie
                 {
                     MovieId = 10,
-                    MovieName = "Scarlet Witch",
+                    MovieName = "WandaVision",
                     VideoPath = "Video/ScarletWitch.mp4",
-                    Description = "So cool",
-                    Duration = 120,
-                    Rated = 12,
-                    PublishedYear = 2001,
+                    Description = "WandaVision is an American television miniseries written by Jac Schaeffer for the Disney+ streaming service, based on the Marvel Comics characters Wanda Maximoff / Scarlet Witch and Vision.",
+                    Duration = 129,
+                    Rated = 18,
+                    PublishedYear = 2021,
                     Country = "America",
                     ImagePath = "Image/ScarletWitch.jpg"
                 }
