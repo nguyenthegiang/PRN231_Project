a94d7d803437dbe3aec0e1122696e2cd713b4875
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-07-19 17:18:15 +0700

    get all actors

--- PRN231_Project/WebAPI/Controllers/ActorController.cs
+++ PRN231_Project/WebAPI/Controllers/ActorController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Authentication;
+using WebAPI.IRepository;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -11,6 +14,14 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ActorController : ControllerBase
     {
+        private IActorRepository repository;
 
+        public ActorController(IActorRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Actor>> GetActors() => repository.GetListActors();
     }
 }
--- PRN231_Project/WebAPI/DataAccess/ActorDAO.cs
+++ PRN231_Project/WebAPI/DataAccess/ActorDAO.cs
@@ -13,31 +13,16 @@ namespace WebAPI.DataAccess
             List<Actor> actors;
             try
             {
-                MyDbContext context = new MyDbContext();
-                actors = context.Actors.ToList();
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-            return actors;
-        }
-
-        public static Actor GetActorByMovieId(int id)
-        {
-            Actor actor;
-            try
-            {
                 using (var context = new MyDbContext())
                 {
-                    actor = context.Actors.SingleOrDefault(u => u.UserId == id);
+                    actors = context.Actors.ToList();
                 }
             }
             catch (Exception e)
             {
                 throw new Exception(e.Message);
             }
-            return actor;
+            return actors;
         }
     }
 }
