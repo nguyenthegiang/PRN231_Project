b7a3734cf393f823569763953f9efb551827ecbd
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-07-19 16:37:32 +0700

    add ActorDAO

---
+++ PRN231_Project/WebAPI/DataAccess/ActorDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.DataAccess
+{
+    public class ActorDAO
+    {
+        /*public static List<Actor> GetActors()
+        {
+            List<Actor> actors;
+            try
+            {
+                using (var context = new MyDbContext())
+                {
+
+                    actors = context.Actors.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return actors;
+        }
+
+        public static Actor GetActorByMovieId(int id)
+        {
+            Actor actor;
+            try
+            {
+                using (var context = new MyDbContext())
+                {
+                    actor = context.Actors.SingleOrDefault(u => u.UserId == id);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return actor;
+        }*/
+    }
+}
