5b5422c24ed2ecde3f42d823d05231b6c0d2e190
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-07-23 18:01:57 +0700

    [WebClient] {Home} (User) Movie Buttons: Style

--- PRN231_Project/WebClient/js/index.js
+++ PRN231_Project/WebClient/js/index.js
@@ -30,33 +30,14 @@ function GetAllMovies() {
             '<span class="movie-list-item-title">' +
             value["movieName"] +
             "</span>" +
-            '<a href="./WatchMovie.html?id=' +
+            // "<p class=\"movie-list-item-desc\">" +
+            // "</p>" +
+            '<a type="button" class="btn btn-light movie-list-item-watch-button" href="./WatchMovie.html?id=' +
             value["movieId"] +
-            '" class="movie-list-item-watch-button">Watch</a>' +
+            '"><i class="movie-item-button-icon fa-solid fa-play"></i></a>' +
             "</div>"
         );
       });
-
-      //Paging
-      // const arrows = document.querySelectorAll(".arrow");
-      // const movieLists = document.querySelectorAll(".movie-list");
-
-      // arrows.forEach((arrow, i) => {
-      //   const itemNumber = movieLists[i].querySelectorAll("img").length;
-      //   let clickCounter = 0;
-      //   arrow.addEventListener("click", () => {
-      //     const ratio = Math.floor(window.innerWidth / 270);
-      //     clickCounter++;
-      //     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      //       movieLists[i].style.transform = `translateX(${
-      //         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
-      //       }px)`;
-      //     } else {
-      //       movieLists[i].style.transform = "translateX(0)";
-      //       clickCounter = 0;
-      //     }
-      //   });
-      // });
     },
     error: function (xhr, status, error) {
       console.log(xhr);
@@ -84,24 +65,11 @@ function GetMoviesByCategory(categoryId) {
             '<span class="movie-list-item-title">' +
             value["movieName"] +
             "</span>" +
-            // "<p class=\"movie-list-item-desc\">" +
-            // "</p>" +
             '<a href="./WatchMovie.html?id=' +
             value["movieId"] +
             '" class="movie-list-item-watch-button">Watch</a>' +
             "</div>"
         );
-
-        // appendElement.append($("<td>").html(value["movieId"]));
-        // appendElement.append($("<td>").html(value["movieName"]));
-        // appendElement.append($("<td>").html(value["videoPath"]));
-        // appendElement.append($("<td>").html(value["description"]));
-        // appendElement.append($("<td>").html(value["duration"]));
-        // appendElement.append($("<td>").html(value["rated"]));
-        // appendElement.append($("<td>").html(value["publishedYear"]));
-        // appendElement.append($("<td>").html(value["country"]));
-        // appendElement.append($("<td>")
-        //     .html("<img src=\"image\\movies\\" + value["imagePath"] + "\" width=\"100\" />"));
       });
 
       //Paging
@@ -151,7 +119,7 @@ function GetFeaturedMovie(id) {
           '<p class="featured-desc">' +
           result["description"] +
           "</p>" +
-          '<a type="button" class="btn btn-light featured-button" href="./WatchMovie.html?id=' +
+          '<a type="button" class="btn btn-light featured-button featured-play-button" href="./WatchMovie.html?id=' +
           result["movieId"] +
           '"><i class="featured-button-icon fa-solid fa-play"></i> Play</a>' +
           '<a type="button" class="btn btn-secondary featured-button" href="">' +
